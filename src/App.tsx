72f58118a2a1e340249d4660ceb63ac102711e0b
rbpolim <rodrigobpolim@gmail.com>
2021-06-22 20:57:33 -0300

    aula-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
